43d338f4d9c99cb214711222bd5d78cf24065280
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-22 15:01:38 +0700

    feat(App): Update App to add Home for currency

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import "./App.css";
 // import Home from "./components/home.component";
 import React from "react";
-import { Clock } from "./components/realtime-clock";
-import { Users } from "./components/random-users";
+// import { Clock } from "./components/realtime-clock";
+// import { Users } from "./components/random-users";
+import Home from "./components/home.component";
 
 function App() {
   return (
-    // <Home />
-    <div>
-      <Clock />
-      <Users />
-    </div>
+    <Home />
+    // <div>
+    //   <Clock />
+    //   <Users />
+    // </div>
   );
 }
 
